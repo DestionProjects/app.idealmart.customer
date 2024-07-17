ca16141c20ac2cc5629c15b6683925baf9bb2767
shankar samidala <shankar@kalpas.in>
2024-07-17 16:31:05 +0530

    new

--- lib/features/login/presentation/pages/sign_up.dart
+++ lib/features/login/presentation/pages/sign_up.dart
@@ -1,3 +1,5 @@
+// lib/features/login/presentation/pages/sign_up.dart
+
 import 'package:flutter/material.dart';
 import 'package:idealmart_customer/constants/custom_widgets.dart';
 import 'package:idealmart_customer/constants/dimensions.dart';
@@ -7,93 +9,157 @@ import 'package:idealmart_customer/features/login/presentation/pages/congratulat
 import 'package:idealmart_customer/features/login/presentation/pages/sign_in.dart';
 import 'package:idealmart_customer/features/login/presentation/widgets/login_widgets.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
   const SignUp({super.key});
 
   @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController pinCodeController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isAgreed = false;
+
+  @override
   Widget build(BuildContext context) {
-    // final SignUpController controller = Get.put(SignUpController());
-    final TextEditingController nameController = TextEditingController();
-    final TextEditingController emailController = TextEditingController();
-    final TextEditingController phoneController = TextEditingController();
-    final TextEditingController pinCodeController = TextEditingController();
-    final TextEditingController passwordController = TextEditingController();
     return Scaffold(
-        backgroundColor: CustomColors.white,
-        body: Stack(
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: SingleChildScrollView(
-                  child: Column(children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 35),
-                      child: Align(
-                          alignment: Alignment.center,
-                          child: LoginWidgets.headerText('Create new account')),
+      backgroundColor: CustomColors.white,
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Add the image widget here
+                    Center(
+                      child: Image.asset(
+                        'assets/logos/logo_dark.png', // Replace with your image URL or asset
+                        width:
+                            150, // Set the width according to your requirement
+                        height:
+                            150, // Set the height according to your requirement
+                      ),
                     ),
-                    // Name
+                    const SizedBox(height: 10),
                     Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text('Full Name', style: TextStyles.styleMB)),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Welcome to !DealMart',
+                        style: TextStyles.styleELBB,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Sign up to explore deals',
+                        style: TextStyles.styleMB,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    // Name
+                    Text('Full Name', style: TextStyles.styleMB),
                     const SizedBox(height: 4),
                     LoginWidgets.nameTextField(nameController),
                     const SizedBox(height: 20),
                     // Email
-                    Align(
-                        alignment: Alignment.centerLeft,
-                        child: LoginWidgets.loginText('Email')),
+                    Text('Email', style: TextStyles.styleMB),
                     const SizedBox(height: 4),
                     LoginWidgets.emailTextField(emailController),
                     const SizedBox(height: 20),
-                    Align(
-                        alignment: Alignment.centerLeft,
-                        child: LoginWidgets.loginText('Phone')),
+                    // Phone
+                    Text('Phone Number', style: TextStyles.styleMB),
                     const SizedBox(height: 4),
                     LoginWidgets.phoneTextField(phoneController),
                     const SizedBox(height: 20),
-                    Align(
-                        alignment: Alignment.centerLeft,
-                        child: LoginWidgets.loginText('Pin Code')),
+                    // Pin Code
+                    Text('Pin Code', style: TextStyles.styleMB),
                     const SizedBox(height: 4),
                     LoginWidgets.pinCodeTextField(pinCodeController),
                     const SizedBox(height: 20),
                     // Password
-                    Align(
-                        alignment: Alignment.centerLeft,
-                        child: LoginWidgets.loginText('Password')),
+                    Text('Password', style: TextStyles.styleMB),
                     const SizedBox(height: 4),
                     PasswordTextField(controller: passwordController),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 35),
-                      child: CustomWidgets.customButton('Sign up', () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>const CongratulationPage(text: 'Your account has been created')));
+                    const SizedBox(height: 20),
+                    // Terms and Conditions
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: isAgreed,
+                          activeColor:
+                              CustomColors.green, // Set the active color
+                          onChanged: (value) {
+                            setState(() {
+                              isAgreed = value!;
+                            });
+                          },
+                        ),
+                        Expanded(
+                          child: Text(
+                            'By signing up, you agree to our Terms & Conditions and Privacy Policy',
+                            style: TextStyles.styleSB,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    // Sign Up Button
+                    Center(
+                      child: CustomWidgets.customButton('Sign Up', () {
+                        if (isAgreed) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const CongratulationPage(
+                                text: 'Your account has been created',
+                              ),
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                  'You must agree to the terms and conditions to sign up.'),
+                            ),
+                          );
+                        }
                       }),
                     ),
-                    Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: LoginWidgets.textAndTextButton(
-                            'Already have an account ', 'Sign in', () {
+                    const SizedBox(height: 20),
+                    // Already have an account
+                    Center(
+                      child: LoginWidgets.textAndTextButton(
+                        'Already have an account? ',
+                        'Login',
+                        () {
                           Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignIn()));
-                        }, TextStyles.styleMB, Dimensions.largeTextSize)),
-                    Padding(
-                        padding: const EdgeInsets.only(top: 25),
-                        child: LoginWidgets.textAndTextButton(
-                            'By Signup you agree to the ',
-                            'Terms & Conditions',
-                            () {},
-                            TextStyles.styleSB,
-                            Dimensions.smallTextSize)),
-                  ]),
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SignIn(),
+                            ),
+                          );
+                        },
+                        TextStyles.styleMB,
+                        Dimensions.largeTextSize,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                  ],
                 ),
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/features/login/presentation/widgets/login_widgets.dart
+++ lib/features/login/presentation/widgets/login_widgets.dart
@@ -1,98 +1,121 @@
+// lib/features/login/presentation/widgets/login_widgets.dart
+
 import 'package:flutter/material.dart';
 import 'package:idealmart_customer/constants/fonts_colors.dart';
 import 'package:idealmart_customer/constants/text_styles.dart';
 
-class LoginWidgets{
-  static  Widget emailTextField(TextEditingController controller) {
+class LoginWidgets {
+  static Widget emailTextField(TextEditingController controller) {
     return TextField(
       controller: controller,
       style: TextStyles.styleMB,
       decoration: InputDecoration(
+        hintText: 'Enter your email',
         enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
-            borderRadius: BorderRadius.circular(6)),
+          borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
+          borderRadius: BorderRadius.circular(6),
+        ),
         focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
-            borderRadius: BorderRadius.circular(6)),
+          borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
+          borderRadius: BorderRadius.circular(6),
+        ),
       ),
     );
   }
-  static  Widget nameTextField(TextEditingController controller) {
+
+  static Widget nameTextField(TextEditingController controller) {
     return TextField(
       controller: controller,
       style: TextStyles.styleMB,
       decoration: InputDecoration(
+        hintText: 'Enter your full name',
         enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
-            borderRadius: BorderRadius.circular(6)),
+          borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
+          borderRadius: BorderRadius.circular(6),
+        ),
         focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
-            borderRadius: BorderRadius.circular(6)),
+          borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
+          borderRadius: BorderRadius.circular(6),
+        ),
       ),
     );
   }
-  static  Widget phoneTextField(TextEditingController controller) {
+
+  static Widget phoneTextField(TextEditingController controller) {
     return TextField(
       controller: controller,
       style: TextStyles.styleMB,
       decoration: InputDecoration(
+        hintText: 'Enter your phone number',
         enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
-            borderRadius: BorderRadius.circular(6)),
+          borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
+          borderRadius: BorderRadius.circular(6),
+        ),
         focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
-            borderRadius: BorderRadius.circular(6)),
+          borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
+          borderRadius: BorderRadius.circular(6),
+        ),
       ),
     );
   }
-  static  Widget pinCodeTextField(TextEditingController controller) {
+
+  static Widget pinCodeTextField(TextEditingController controller) {
     return TextField(
       controller: controller,
       style: TextStyles.styleMB,
       decoration: InputDecoration(
+        hintText: 'Enter your pin code',
         enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
-            borderRadius: BorderRadius.circular(6)),
+          borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
+          borderRadius: BorderRadius.circular(6),
+        ),
         focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
-            borderRadius: BorderRadius.circular(6)),
+          borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
+          borderRadius: BorderRadius.circular(6),
+        ),
       ),
     );
   }
-  static Widget textAndTextButton(String text, String textButton, VoidCallback onTap, TextStyle textStyle, double textButtonSize){
+
+  static Widget textAndTextButton(String text, String textButton,
+      VoidCallback onTap, TextStyle textStyle, double textButtonSize) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         Text(
-         text,
-          style:textStyle,
+          text,
+          style: textStyle,
         ),
         GestureDetector(
           onTap: onTap,
-          child: Text(textButton,
-              style: TextStyle(
-                  decoration: TextDecoration.underline,
-                  decorationColor: CustomColors.green,
-                  color: CustomColors.green,
-                  fontFamily: Fonts.poppins,
-                  fontSize: textButtonSize)),
+          child: Text(
+            textButton,
+            style: TextStyle(
+              decoration: TextDecoration.underline,
+              decorationColor: CustomColors.green,
+              color: CustomColors.green,
+              fontFamily: Fonts.poppins,
+              fontSize: textButtonSize,
+            ),
+          ),
         ),
       ],
     );
   }
-  static Widget loginText(String text){
+
+  static Widget loginText(String text) {
     return Text(text, style: TextStyles.styleMB);
   }
-  static Widget headerText(String text){
-    return Text(text,style: TextStyles.styleELBB);
-  }
 
+  static Widget headerText(String text) {
+    return Text(text, style: TextStyles.styleELBB);
+  }
 }
+
 class PasswordTextField extends StatefulWidget {
-  const PasswordTextField({
-    super.key, required this.controller,
-  });
+  const PasswordTextField({super.key, required this.controller});
+
   final TextEditingController controller;
 
   @override
@@ -105,22 +128,28 @@ class _PasswordTextFieldState extends State<PasswordTextField> {
   @override
   Widget build(BuildContext context) {
     return TextField(
-      style:TextStyles.styleMB,
+      style: TextStyles.styleMB,
       controller: widget.controller,
       obscureText: !_isPasswordVisible,
       obscuringCharacter: '*',
       decoration: InputDecoration(
+        hintText: 'Enter your password',
         filled: true,
         fillColor: CustomColors.white,
         enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
-            borderRadius: BorderRadius.circular(6)),
+          borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
+          borderRadius: BorderRadius.circular(6),
+        ),
         focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
-            borderRadius: BorderRadius.circular(6)),
+          borderSide: BorderSide(color: CustomColors.grey.withOpacity(0.5)),
+          borderRadius: BorderRadius.circular(6),
+        ),
         suffixIcon: IconButton(
           icon: Icon(
-              _isPasswordVisible ? Icons.visibility_outlined: Icons.visibility_off_outlined),
+            _isPasswordVisible
+                ? Icons.visibility_outlined
+                : Icons.visibility_off_outlined,
+          ),
           onPressed: () {
             setState(() {
               _isPasswordVisible = !_isPasswordVisible;
